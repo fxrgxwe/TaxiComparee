014e735437eeffd9b52a20810aa6193a9d73217f
fxrgxwe <fxrgxwe@gmail.com>
2025-06-09 23:51:31 +0300

    new test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/TaxiComparee",
   plugins: [react(), tailwindcss()],
-  base: "/TaxiComparee"
 })
